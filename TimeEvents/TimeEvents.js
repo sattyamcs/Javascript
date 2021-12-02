fed80e428945cad3edb4414b2796fdce17cf07fb
sattyam <sattyamthakur02@gmail.com>
2021-12-02 23:25:59 +0530

    newly commited

---
+++ TimeEvents/TimeEvents.js
@@ -0,0 +1,8 @@
+// The window object allows execution of code at the specified time intervals
+// The time intervals are called timing events
+// There are two methods 
+// 1. setTimeout(function,milliseconds); //executes a function, after waiting for specified number of milliseconds
+
+
+// 2. setinterval(function, milliseconds); // same as timeout, but repeats the execution ;of the function continuosly
+
